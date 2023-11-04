7952a086c53d5b24de7eb45caaf38dd980620bd1
nipplelion <piolotorrico@yahoo.com>
2023-11-04 13:23:54 -0700

    feat: Enemy just moves in a straight line toward the player if no path

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Tilemaps;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent agent;
+
+    public float speed;
+
+    private float distance;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,22 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position); 
+
+        NavMeshPath path = new NavMeshPath();
+        agent.CalculatePath(target.position, path);
+
+        if (path.status == NavMeshPathStatus.PathComplete)
+        {
+            Debug.Log("Has Path: " + agent.hasPath);
+            agent.SetDestination(target.position);
+        } else
+        {
+            distance = Vector2.Distance(transform.position, target.position);
+            Vector2 direction = target.position - transform.position;
+
+            transform.position = Vector2.MoveTowards(this.transform.position, target.position, speed * Time.deltaTime);
+        }
+        
     }
+
 }
