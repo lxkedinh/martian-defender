089de6753a24f7c514524a7f44e8bb9909043da8
joshuaereyes <joshuareyes562@yahoo.com>
2023-12-09 09:40:24 -0800

    health bar was added to enemy ai
    added health bar to enemy

--- Assets/Scripts/Components/Health.cs
+++ Assets/Scripts/Components/Health.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public int health;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        
+    }
+
 
     public void TakeDamage(Attack attack)
     {
-        health -= attack.attackDamage;
-        if (health <= 0)
+        currentHealth -= attack.attackDamage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
