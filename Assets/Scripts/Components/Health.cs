bb86d5dc62ef6c652226053cfcca5da20c17ecc4
Luke Dinh <lukexdinh@gmail.com>
2023-12-14 01:03:55 -0800

    feat: mine tooltip when nearby ores and ship health UI

--- Assets/Scripts/Components/Health.cs
+++ Assets/Scripts/Components/Health.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     public int health;
+    public UnityEvent onDeath;
 
     public void TakeDamage(Attack attack)
     {
         health -= attack.attackDamage;
         if (health <= 0)
         {
+            onDeath?.Invoke();
             Destroy(gameObject);
         }
     }
